ed7ee5233799050a57f848c9eb685a56df7d01f1
FajarRamadhanBBX <fjrrmdhn411@gmail.com>
2025-04-12 10:25:03 +0700

    Add splash screen & auth page

--- app/src/main/java/com/sonalune/pbp/view/activities/ActivityAuth.java
+++ app/src/main/java/com/sonalune/pbp/view/activities/ActivityAuth.java
@@ -1,6 +1,13 @@
 package com.sonalune.pbp.view.activities;
 
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,54 +17,60 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.sonalune.pbp.R;
 
-public class ActivityAuth extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_auth);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
-    }
-}
-
-//public class AuthActivity extends AppCompatActivity {
-//
-//    LinearLayout layoutSignUp, layoutSignIn;
-//    Button btnTabSignUp, btnTabSignIn;
+//public class ActivityAuth extends AppCompatActivity {
+//    
+//    LinearLayout layoutSignUp
 //
 //    @Override
 //    protected void onCreate(Bundle savedInstanceState) {
 //        super.onCreate(savedInstanceState);
+//        EdgeToEdge.enable(this);
 //        setContentView(R.layout.activity_auth);
-//
-//        layoutSignUp = findViewById(R.id.layoutSignUp);
-//        layoutSignIn = findViewById(R.id.layoutSignIn);
-//        btnTabSignUp = findViewById(R.id.btnTabSignUp);
-//        btnTabSignIn = findViewById(R.id.btnTabSignIn);
-//
-//        btnTabSignUp.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                layoutSignUp.setVisibility(View.VISIBLE);
-//                layoutSignIn.setVisibility(View.GONE);
-//                btnTabSignUp.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#D14F72")));
-//                btnTabSignIn.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FDD1D9")));
-//            }
-//        });
-//
-//        btnTabSignIn.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                layoutSignUp.setVisibility(View.GONE);
-//                layoutSignIn.setVisibility(View.VISIBLE);
-//                btnTabSignUp.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FDD1D9")));
-//                btnTabSignIn.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#D14F72")));
-//            }
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.auth), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
 //        });
 //    }
 //}
+
+public class ActivityAuth extends AppCompatActivity {
+    LinearLayout layoutSignUp, layoutSignIn;
+    Button btnTabSignUp, btnTabSignIn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auth);
+
+        layoutSignUp = findViewById(R.id.layoutSignUp);
+        layoutSignIn = findViewById(R.id.layoutSignIn);
+        btnTabSignUp = findViewById(R.id.btnTabSignUp);
+        btnTabSignIn = findViewById(R.id.btnTabSignIn);
+
+        btnTabSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layoutSignUp.setVisibility(View.VISIBLE);
+                layoutSignIn.setVisibility(View.GONE);
+                Color Color = null;
+                btnTabSignUp.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#D14D72")));
+                btnTabSignUp.setTextColor(ColorStateList.valueOf(Color.parseColor("#FFFFFF")));
+                btnTabSignIn.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FCC8D1")));
+                btnTabSignIn.setTextColor(ColorStateList.valueOf(Color.parseColor("#D14D72")));
+            }
+        });
+
+        btnTabSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layoutSignUp.setVisibility(View.GONE);
+                layoutSignIn.setVisibility(View.VISIBLE);
+                btnTabSignIn.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#D14D72")));
+                btnTabSignIn.setTextColor(ColorStateList.valueOf(Color.parseColor("#FFFFFF")));
+                btnTabSignUp.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#FCC8D1")));
+                btnTabSignUp.setTextColor(ColorStateList.valueOf(Color.parseColor("#D14D72")));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/sonalune/pbp/view/activities/SplashScreen.java
@@ -0,0 +1,39 @@
+package com.sonalune.pbp.view.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.sonalune.pbp.R;
+
+public class SplashScreen extends AppCompatActivity {
+    private int loading_time = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent auth = new Intent(SplashScreen.this, ActivityAuth.class);
+                startActivity(auth);
+                finish();
+            }
+        }, loading_time);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
