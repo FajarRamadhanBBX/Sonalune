0af216e9195bb14600ede10af19dde0922a707af
Krauchelli <mekaku26shi@gmail.com>
2025-04-12 21:54:47 +0700

    refactor: add slicing adddition for home fragment

--- app/src/main/java/com/sonalune/pbp/view/activities/MainActivity.java
+++ app/src/main/java/com/sonalune/pbp/view/activities/MainActivity.java
@@ -7,20 +7,64 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.sonalune.pbp.R;
+import com.sonalune.pbp.view.fragments.HomeFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNav;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        bottomNav = findViewById(R.id.bottom_nav);
+
+        // Load default fragment
+        loadFragment(new HomeFragment());
+
+        // Set nav listener
+        bottomNav.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+
+            if (item.getItemId() == R.id.nav_home) {
+                selectedFragment = new HomeFragment();
+                //                case R.id.nav_playlist:
+//                    selectedFragment = new PlaylistFragment();
+//                    break;
+//                case R.id.nav_capsule:
+//                    selectedFragment = new CapsuleFragment();
+//                    break;
+            }
+// ubah nanti
+//            switch (item.getItemId()) {
+//                case R.id.nav_home:
+//                    selectedFragment = new HomeFragment();
+//                    break;
+////                case R.id.nav_playlist:
+////                    selectedFragment = new PlaylistFragment();
+////                    break;
+////                case R.id.nav_capsule:
+////                    selectedFragment = new CapsuleFragment();
+////                    break;
+//            }
+
+            return loadFragment(selectedFragment);
         });
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
     }
 }
